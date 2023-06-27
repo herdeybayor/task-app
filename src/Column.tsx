486b3719421218c9c95b2a5ab8685e8450c02b7b
herdeybayor <shereefdeen4real@gmail.com>
2023-06-27 11:47:01 +0100

    chore: customize appearance ♻

--- src/Column.tsx
+++ src/Column.tsx
@@ -11,8 +11,8 @@ const Column = ({ column: { title, id }, tasks }: Props) => {
         <div className="m-2 border rounded-sm">
             <h3 className="p-2 font-medium text-2xl">{title}</h3>
             <Droppable droppableId={id}>
-                {({ droppableProps, innerRef, placeholder }) => (
-                    <div className="p-2" {...droppableProps} ref={innerRef}>
+                {({ droppableProps, innerRef, placeholder }, { isDraggingOver }) => (
+                    <div className={`p-2 transition-colors duration-200 ease-in ${isDraggingOver ? "bg-blue-300" : "bg-white"}`} {...droppableProps} ref={innerRef}>
                         {tasks.map((task, i) => (
                             <Task key={task.id} task={task} index={i} />
                         ))}
--- src/Task.tsx
+++ src/Task.tsx
@@ -8,8 +8,8 @@ type Props = {
 const Task = ({ task, index }: Props) => {
     return (
         <Draggable draggableId={task.id} index={index}>
-            {({ dragHandleProps, draggableProps, innerRef }) => (
-                <div ref={innerRef} {...draggableProps} {...dragHandleProps} className="border p-2 mb-2 rounded-sm bg-white">
+            {({ dragHandleProps, draggableProps, innerRef }, { isDragging }) => (
+                <div ref={innerRef} {...draggableProps} {...dragHandleProps} className={`border p-2 mb-2 rounded-sm ${isDragging ? "bg-green-300" : "bg-white"}`}>
                     {task.content}
                 </div>
             )}
