5e5c9bbb5338ce2ae85fbdcbe1d633b6e8c87f89
herdeybayor <shereefdeen4real@gmail.com>
2023-06-27 11:29:34 +0100

    chore: persist data onDragEnd ♻

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,62 @@ import initialData from "./initial-data";
 import Column from "./Column";
 
 function App() {
-    const [data] = React.useState(initialData);
+    const [data, setData] = React.useState(initialData);
 
-    const onDragEnd = (result: DropResult) => {
-        console.log(result);
+    const onDragEnd = ({ destination, source, draggableId }: DropResult) => {
+        if (!destination) return;
+        if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+        const start = data.columns[source.droppableId];
+        const finish = data.columns[destination.droppableId];
+
+        if (start === finish) {
+            const newTaskIds = Array.from(start.taskIds);
+            newTaskIds.splice(source.index, 1);
+            newTaskIds.splice(destination.index, 0, draggableId);
+
+            const newColumn = {
+                ...start,
+                taskIds: newTaskIds,
+            };
+
+            const newState = {
+                ...data,
+                columns: {
+                    ...data.columns,
+                    [newColumn.id]: newColumn,
+                },
+            };
+
+            setData(newState);
+            return;
+        } else {
+            const startTaskIds = Array.from(start.taskIds);
+            startTaskIds.splice(source.index, 1);
+            const newStart = {
+                ...start,
+                taskIds: startTaskIds,
+            };
+
+            const finishTaskIds = Array.from(finish.taskIds);
+            finishTaskIds.splice(destination.index, 0, draggableId);
+            const newFinish = {
+                ...finish,
+                taskIds: finishTaskIds,
+            };
+
+            const newState = {
+                ...data,
+                columns: {
+                    ...data.columns,
+                    [newStart.id]: newStart,
+                    [newFinish.id]: newFinish,
+                },
+            };
+
+            setData(newState);
+            return;
+        }
     };
     return (
         <DragDropContext onDragEnd={onDragEnd}>
--- src/initial-data.ts
+++ src/initial-data.ts
@@ -4,6 +4,10 @@ const initialData: IData = {
         "task-2": { id: "task-2", content: "Watch my favorite show" },
         "task-3": { id: "task-3", content: "Charge my phone" },
         "task-4": { id: "task-4", content: "Cook dinner" },
+        "task-5": { id: "task-5", content: "Walk the dog" },
+        "task-6": { id: "task-6", content: "Clean the car" },
+        "task-7": { id: "task-7", content: "Clean the house" },
+        "task-8": { id: "task-8", content: "Clean the yard" },
     },
     columns: {
         "column-1": {
@@ -11,9 +15,19 @@ const initialData: IData = {
             title: "To do",
             taskIds: ["task-1", "task-2", "task-3", "task-4"],
         },
+        "column-2": {
+            id: "column-2",
+            title: "In progress",
+            taskIds: ["task-5", "task-6"],
+        },
+        "column-3": {
+            id: "column-3",
+            title: "Done",
+            taskIds: ["task-7", "task-8"],
+        },
     },
     // Facilitate reordering of the columns
-    columnOrder: ["column-1"],
+    columnOrder: ["column-1", "column-2", "column-3"],
 };
 
 export default initialData;
